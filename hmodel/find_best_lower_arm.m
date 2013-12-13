62480c1f96000dcec943ab8232e03138feb54af5
Kumar Shaurya Shankar <icoderaven@gmail.com>
2013-12-13 08:15:32 -0500

    Final commits.

--- hmodel/find_best_lower_arm.m
+++ hmodel/find_best_lower_arm.m
@@ -17,37 +17,38 @@ seed_traj = stim_trajectories.data(1:step_interval:end,:);
 % seed_traj = taus.data(1:step_interval:end,:);
 x0 = reshape(seed_traj, numel(seed_traj),1);
 ub = 1*ones(size(x0));
-lb = -1*ones(size(x0));
+lb = 0*ones(size(x0));
 opt.algorithm = NLOPT_GN_DIRECT_L;
 opt.min_objective = @(x) evaluate_neuro_cost(x, target, lambda, step_interval, stim_trajectories);
 opt.lower_bounds = lb;
 opt.upper_bounds = ub;
-results = cell(100);
-new_x0s = cell(100);
-costs = cell(100);
-for i=1:numel(results)
-    cost = 1e16;
-    %Check if this is a valid seed!!!!
-    while cost>1e13
-        new_x0 = x0+ 0.05.*randn(numel(x0),1);
-        new_x0(new_x0>1) = 1;
-        new_x0(new_x0<-1) = -1;
-        cost = evaluate_neuro_cost(new_x0, target, lambda, step_interval, stim_trajectories);
-    end
-    costs{i} = cost;
-    new_x0s{i} = new_x0;
-end
-
+results = cell(100,1);
+new_x0s = cell(100,1);
+costs = cell(100,1);
+% for i=1:numel(results)
+%     cost = 1e16;
+%     %Check if this is a valid seed!!!!
+%     while cost>1e13
+%         new_x0 = x0+ 0.05.*randn(numel(x0),1);
+%         new_x0(new_x0>1) = 1;
+%         new_x0(new_x0<0) = 0;
+%         cost = evaluate_neuro_cost(new_x0, target, lambda, step_interval, stim_trajectories);
+%     end
+%     costs{i} = cost;
+%     new_x0s{i} = new_x0;
+% end
+% save('temp.mat', 'costs', 'new_x0s');
 %Find the min cost traj, use that as seed for minimization
-index = min(costs);
-x0 = new_x0s{index};
+% index = min(cell2mat(costs));
+% x0 = new_x0s{index};
+load('temp.mat');
 for i=1:numel(results)
     cost = 1e16;
     %Check if this is a valid seed!!!!
     while cost>1e13
-        new_x0 = x0 + 0.05.*randn(numel(x0),1);
+        new_x0 = x0 + 0.01.*randn(numel(x0),1);
         new_x0(new_x0>1) = 1;
-        new_x0(new_x0<-1) = -1;
+        new_x0(new_x0<0) = 0;
         cost = evaluate_neuro_cost(new_x0, target, lambda, step_interval, stim_trajectories);
     end
     new_x0
@@ -56,4 +57,6 @@ for i=1:numel(results)
     [xopt, fmin, retcode] = nlopt_optimize(opt,new_x0);
     results{i,1} = best_traj;
     results{i,2} = fval;
+    name = sprintf('iter_%d.mat', i);
+    save(name, 'best_traj', 'fval');
 end
